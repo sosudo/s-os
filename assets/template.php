b6c0ceb9d9c85de655081f2e72995ea35279867a
Imperial-Industries <shivansh.gour90@gmail.com>
2021-04-29 19:07:06 -0400

    a

--- assets/template.php
+++ assets/template.php
@@ -9,9 +9,11 @@
 	<link rel="icon"
       type="image/png"
       href="media/images/favicon.svg">
-	<!-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css"> -->
-	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700|Roboto+Mono">
+	<!-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css"> 
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700|Roboto+Mono">-->
 	<link rel="stylesheet" type="text/css" href="assets/styles/style.css?">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500&display=swap" rel="stylesheet">
 	<script src="assets/main.js"></script>
 </head>
 
